20f921a7ba007e2e095bd85e335dc508af6b6b1f
christopheralan88 <christopheralan88@gmail.com>
2017-02-15 00:14:29 -0500

    wrote and ran second test

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -5,23 +5,27 @@ import org.junit.Test;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
 public class UserTest {
 
     private Board board;
+    private User me;
+    private User anotherUser;
+    private Question question;
 
     @Before
     public void setUp() throws Exception {
         board = new Board("Java");
+        me = new User(board, "me");
+        anotherUser = new User(board, "another user");
+        question = me.askQuestion("What is your favorite programming language?");
     }
 
     @Test
-    public void usersReputationIncreasesWhenQuestionIsUpvoted() throws Exception {
+    public void userReputationIncreasesWhenQuestionIsUpvoted() throws Exception {
         // Arrange
-        User me = new User(board, "me");
-        User anotherUser = new User(board, "another user");
-        Question question = me.askQuestion("What is your favorite programming language?");
 
         // Act (method being tested is the User.upVote() method)
         anotherUser.upVote(question);
@@ -29,5 +33,18 @@ public class UserTest {
 
         // Assert
         assertEquals(5, reputation);
+    }
+
+    @Test
+    public void userReputationIncreasesWhenAnswerIsUpvoted() {
+        // Arrange
+        Answer answer = anotherUser.answerQuestion(question, "Python");
+
+        // check if answerer's reputation goes up
+        me.upVote(answer);
+        int reputation = anotherUser.getReputation();
+
+        // Assert
+        assertEquals(10, reputation);
     }
 }
